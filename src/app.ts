addc4c443cdca2c79915351ef4f00e67d6d66203
sz928 <18230172084@163.com>
2020-08-13 10:12:11 +0800

    fix

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-    res.send('hello world!')
-})
-app.listen(port, () => { console.log('已经启动: http://localhost:' + port + '/') });
